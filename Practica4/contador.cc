21f8ebfc73e4031702141bb52761ca6ff9f1e00b
chema969 <chema969@hotmail.com>
2017-11-20 13:49:49 +0100

    danitico

--- Practica4/contador.cc
+++ Practica4/contador.cc
@@ -1,29 +1,83 @@
 #include "contador.h"
-
+#include <list>
  Contador Contador::operator=(const Contador &c){
     contador_=c.contador_;
-    minimo_=c.minimo_;
-    maximo_=c.maximo_;
+     guarda(contador_);
    return *this;
   }
 
 Contador Contador::operator=(const int &c){
     contador_=c;
+     guarda(contador_);
     return *this;
   }
 
 Contador Contador::operator++(void){
   ++contador_;
+     guarda(contador_);
+  return *this;
+  }
+
+Contador Contador::operator--(void){
+  --contador_;
+     guarda(contador_);
   return *this;
   }
 
 Contador Contador::operator++(int){
   Contador c=*this;
   ++contador_;
+     guarda(contador_);
   return c;
   }
 
-Contador Contador::operator+(const int &c){
-  contador_+=c;
-  return *this;
+Contador Contador::operator--(int){
+  Contador c=*this;
+  --contador_;
+     guarda(contador_);
+  return c;
   }
+
+Contador operator+(const int &a,const Contador &c){
+  Contador x;
+  if((c.contador_+a)<c.maximo_)
+  x.contador_=c.contador_+a;  
+  guarda(x.contador_);
+  return x;
+  }
+
+Contador operator+(const Contador &c,const int &a){
+  Contador x;
+  if((c.contador_+a)<c.maximo_)
+  x.contador_=c.contador_+a; 
+  guarda(x.contador_); 
+  return x;
+  }
+
+Contador operator-(const int &a,const Contador &c){
+  Contador x;
+  if((c.contador_-a)>c.minimo_)
+  x.contador_=c.contador_-a;  
+  guarda(x.contador_);
+  return x;
+  }
+
+Contador operator-(const Contador &c,const int &a){
+  Contador x;
+  if((c.contador_+a)>c.minimo_)
+  x.contador_=c.contador_-a;  
+  guarda(x.contador_);
+  return x;
+  }
+
+Contador Contador::undo(int n){
+  list <int>::iterator it,j;
+  it=aux_.end();
+  for(int i=0;i<n;i++){
+    j=it;
+    it--;
+    aux_.erase(j);
+   }
+  contador_=*it;
+  return *this;
+  }  
--- Practica4/contador.h
+++ Practica4/contador.h
@@ -1,17 +1,25 @@
 #ifndef CONTADOR_H
 #define CONTADOR_H
-
+#include <list>
 class Contador{
    private:
     int contador_,minimo_,maximo_;
+    list <int> aux_;
+    void guarda(int i){aux_.push_back(i);}
    public:
-    Contador(int minimo=0,int maximo=0,int contador=0){minimo_=minimo;maximo_=maximo;contador_=contador;}
-    int getContador(){return contador_;}
+    Contador(int minimo=0,int maximo=1000,int contador=0){minimo_=minimo;maximo_=maximo;contador_=contador;}
+    inline int get()const{return contador_;}
     Contador operator=(const Contador &c);
     Contador operator=(const int &c);
-    friend Contador operator++(void);
-    friend Contador operator++(int);
-    friend Contador operator+(const int &c);
+    Contador operator++(void);
+    Contador operator++(int);
+    Contador operator--(void);
+    Contador operator--(int);
+    friend Contador operator+(const int &a, const Contador &c);
+    friend Contador operator+(const Contador &c,const int &a);
+    friend Contador operator-(const int &a, const Contador &c);
+    friend Contador operator-(const Contador &c,const int &a); 
+    Contador undo(int n);  
 };
 
 
